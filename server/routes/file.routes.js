59e4c001eee6b1a631efeb098422571f6db8027b
Alex Maslk <amksandr@gmail.com>
2023-10-03 20:44:14 +0300

    MDD: Create dir.

--- server/routes/file.routes.js
+++ server/routes/file.routes.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const authMiddleware = require("../middleware/auth.middleware");
-
+const fileController = require("../controllers/fileController");
 
 const router = Router();
 
 // authMiddleware - can use app.use();
-router.post('', authMiddleware);
-router.get('', authMiddleware);
+router.post('', authMiddleware, fileController.createDir);
+router.get('', authMiddleware, fileController.getFiles);
+
+module.exports = router;
